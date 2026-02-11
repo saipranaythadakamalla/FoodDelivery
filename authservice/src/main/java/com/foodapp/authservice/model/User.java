d8e44f53cc883d7ef21ee42ed53cc11a91ffc309
Taruni Priya <tarunipriya1803@gmail.com>
2026-02-11 14:36:54 +0530

    Added code to the  User.java

--- authservice/src/main/java/com/foodapp/authservice/model/User.java
+++ authservice/src/main/java/com/foodapp/authservice/model/User.java
@@ -1,5 +1,50 @@
 package com.foodapp.authservice.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+
 public class User {
-  
+     @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    // Nullable for future (OTP login users may not have password)
+    @Column(nullable = true)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean isVerified = false;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
 }
